91ed788f704a42d1aa442a2e4f889dc9f85a1a4c
ShiroTanya <phamhoanglong.lk1@gmail.com>
2024-05-30 13:17:58 +0700

    Initial commit

---
+++ src/main/java/Tuan3/PhamHoangLong/PhamHoangLongApplication.java
@@ -0,0 +1,13 @@
+package Tuan3.PhamHoangLong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhamHoangLongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhamHoangLongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Tuan3/PhamHoangLong/PhamHoangLongApplicationTests.java
@@ -0,0 +1,13 @@
+package Tuan3.PhamHoangLong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhamHoangLongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
