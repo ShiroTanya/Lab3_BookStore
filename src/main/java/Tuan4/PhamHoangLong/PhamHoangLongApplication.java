e05f75a06fb8e751694013263d7b56b1786abf6e
ShiroTanya <phamhoanglong.lk1@gmail.com>
2024-06-15 14:24:52 +0700

    Connect DB & CRUD

--- src/main/java/Tuan4/PhamHoangLong/PhamHoangLongApplication.java
+++ src/main/java/Tuan4/PhamHoangLong/PhamHoangLongApplication.java
@@ -1,4 +1,4 @@
-package Tuan3.PhamHoangLong;
+package Tuan4.PhamHoangLong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/Tuan4/PhamHoangLong/PhamHoangLongApplicationTests.java
+++ src/test/java/Tuan4/PhamHoangLong/PhamHoangLongApplicationTests.java
@@ -1,4 +1,4 @@
-package Tuan3.PhamHoangLong;
+package Tuan4.PhamHoangLong;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
